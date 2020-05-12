9114d9b9951707a6445526bbfe8a72bfe9a6e909
Jeremiah <jeremiah.towe@gmail.com>
2020-05-12 15:36:03 -0400

    Use post form to create new snippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -46,9 +46,15 @@ func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fiji,\nBut slowly!\n\n- Kobayshi Issa"
-	expires := "7"
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
