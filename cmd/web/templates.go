c0d043a50cbe779bf7aa4ed5a54ea45c37858da3
Jeremiah <jeremiah.towe@gmail.com>
2020-05-12 20:35:54 -0400

    Abstract form validation

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 
+	"jeremiahtowe.com/snippetbox/pkg/forms"
 	"jeremiahtowe.com/snippetbox/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
-	FormData url.Values
-	FormErrors map[string]string
+	Form *forms.Form
 	Snippet *models.Snippet
 	Snippets []*models.Snippet
 }
